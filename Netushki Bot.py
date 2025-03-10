c2b9e27d4f4f932c8be9d90a2c9b3df991a1b61f
Netushki <152712206+Netushki@users.noreply.github.com>
2025-03-10 14:42:03 +0300

    Update Netushki Bot.py

--- Netushki Bot.py
+++ Netushki Bot.py
@@ -34,7 +34,15 @@ used_gifs = {}
 gif_urls = [
     "https://cdn.discordapp.com/attachments/1346943612373569557/1347573414142939279/attachment.gif?ex=67cef40a&is=67cda28a&hm=b1b5b846ec7bf21b1dcb93e393b78dd8dc3dea112e0e8c8d9460308d517e6b84&",
     "https://cdn.discordapp.com/attachments/1322781202851041358/1347037669388980274/attachment.gif",
-    # Добавьте остальные URL-адреса GIF сюда...
+    "https://cdn.discordapp.com/attachments/1309799105756790794/1309909672446398534/speechmemified_Half_Life_Deathmatch_Source.jpg.gif?ex=67cf0af2&is=67cdb972&hm=6cd814dd9f531de8eb5e9a6ecdc7c18ad39c13bbfbd120cbe1e235799d7ee9ea&",
+    "https://tenor.com/view/speech-bubble-gif-26412022",
+    "https://media.discordapp.net/attachments/1055080776808546353/1177601225542352927/attachment.gif?ex=67cee89a&is=67cd971a&hm=54602311db4c7128af5c90aefa3e74a4f78985de1b054016fcb2b694e7f85a4a&",
+    "https://cdn.discordapp.com/attachments/1207730830487855154/1348366855894470697/attachment.gif?ex=67cf33fd&is=67cde27d&hm=b973f19cdfbfbed725153a92c50776435f70ca6181edde63854f3626d93ef791&",
+    "https://cdn.discordapp.com/attachments/1207730830487855154/1348367007401115658/attachment.gif?ex=67cf3422&is=67cde2a2&hm=34336e5ea5a22862ee676ebb2287b053cd686eb64e3b5d70fcfa946dc1037f42&",
+    "https://cdn.jacher.io/f3ac073b88487e1b.gif",
+    "https://cdn.discordapp.com/attachments/1207730830487855154/1348367459392159744/attachment.gif?ex=67cf348d&is=67cde30d&hm=c228de0ae418b42862b759ba41e54a2f434d438f22d465bdfb38c5a89ce49e6e&",
+    "https://cdn.discordapp.com/attachments/1207730830487855154/1348367494766919740/attachment.gif?ex=67cf3496&is=67cde316&hm=5124d61b87c8cd107cdfd91416e273baedd9550eb44d5f37e123e35805ddcfd2&",
+    "https://cdn.discordapp.com/attachments/1207730830487855154/1348367679962091531/attachment.gif?ex=67cf34c2&is=67cde342&hm=60514011f732c063d000e51c4164f1bfcc8f9e395fac4d336b44316aa6d33645&"
 ]
 
 # Обработчик сообщений
@@ -53,13 +61,9 @@ async def on_message(message):
 
         # Выбираем GIF, который еще не был использован этим пользователем
         available_gifs = [gif for gif in gif_urls if gif not in used_gifs.get(user_id, [])]
-
-        # Если нет доступных гифок, сбрасываем их и даем возможность снова выбрать
         if not available_gifs:
-            used_gifs[user_id] = []  # Сбросить использованные GIF
-            available_gifs = gif_urls  # Даем все гифки снова
+            used_gifs[user_id] = []  # Если все гифки использованы, сбрасываем список
 
-        # Выбираем случайную гифку из доступных
         gif_url = random.choice(available_gifs)
 
         # Добавляем выбранный GIF в список использованных
